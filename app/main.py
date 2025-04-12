be32e47e0d653e5eca7b95686f325fa55015bfb6
Sumit Jha <145683169+sumitjha16@users.noreply.github.com>
2025-04-12 11:51:59 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://storybookharry.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
